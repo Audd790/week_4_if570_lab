f2e2c9962cfe57f31ec493b580a83c0170d97fac
auddl <10horysheet10@gmail.com>
2023-03-06 12:56:09 +0700

    Styling and Dice class

--- app/src/main/java/id/ac/umn/diceroller/MainActivity.kt
+++ app/src/main/java/id/ac/umn/diceroller/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+//s
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
